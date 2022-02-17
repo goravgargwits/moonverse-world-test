6d30a35eb371991e322cfca03a48158f800800b6
ohaddahan <ohaddahan@gmail.com>
2022-02-17 08:31:11 +0200

    remove investors area

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,12 @@
-import { Container, PartnersContent, PartnersAndInvestors } from 'styles/pages/home'
-import partnerimagecontainer from 'assets/images/partnerscontainer.png'
+import { Container } from 'styles/pages/home'
+// import { PartnersContent, PartnersAndInvestors } from 'styles/pages/home'
+// import partnerimagecontainer from 'assets/images/partnerscontainer.png'
 import MoonverseInfoWrapper from 'components/home/InfoContainer'
 import ProblemsWrapper from 'components/home/problemContainer'
 import ParticipateWrapper from 'components/home/participateContainer'
 import TokenUtilitiesWrapper from 'components/home/tokenContainer'
-import PartnerWrapper from 'components/home/partnersContainer'
-import InvestorWrapper from 'components/home/invsetorContainer'
+// import PartnerWrapper from 'components/home/partnersContainer'
+// import InvestorWrapper from 'components/home/invsetorContainer'
 import RoadmapWrapper from 'components/home/roadmapContainer'
 import FooterWrapper from 'components/home/footer'
 import VideoWrapper from 'components/home/videoContainer'
@@ -18,13 +19,13 @@ const Home = () => {
       <ProblemsWrapper />
       <ParticipateWrapper />
       <TokenUtilitiesWrapper />
-      <PartnersAndInvestors>
-        <img className="partner_background" src={partnerimagecontainer} />
-        <PartnersContent>
-          <PartnerWrapper />
-          <InvestorWrapper />
-        </PartnersContent>
-      </PartnersAndInvestors>
+      {/*<PartnersAndInvestors>*/}
+      {/*  <img className="partner_background" src={partnerimagecontainer} />*/}
+      {/*<PartnersContent>*/}
+      {/*  <PartnerWrapper />*/}
+      {/*<InvestorWrapper />*/}
+      {/*</PartnersContent>*/}
+      {/*</PartnersAndInvestors>*/}
       <RoadmapWrapper />
       <FooterWrapper />
     </Container>
