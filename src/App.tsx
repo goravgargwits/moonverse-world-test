e37c8c4d877f7002f265e9715cef4b6b161e8cc6
ohaddahan <ohaddahan@gmail.com>
2022-03-21 18:21:34 +0200

    fix AOS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import Home from 'pages/home'
 import './App.css'
+import AOS from 'aos'
+import { useEffect } from 'react'
 
 const App = () => {
+  useEffect(() => {
+    AOS.init({})
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
